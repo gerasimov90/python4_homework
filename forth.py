4bd8bc5e4838735dad0497933af2e362869da16a
GerasimovVictor <gerasimov.viktor90@gmail.com>
2023-03-05 13:07:03 +0700

    task 2

--- forth.py
+++ forth.py
@@ -1,5 +1,5 @@
 #Задача 22
-mol = [int(x) for x in input('Введите через пробел количество элементов в двух множествах: ').split()]
+""" mol = [int(x) for x in input('Введите через пробел количество элементов в двух множествах: ').split()]
 n = mol[0]
 m = mol[1]
 set_1 = set()
@@ -16,4 +16,16 @@ lok = set_1 & set_2
 kool = list(lok)
 kool.sort()
 for i in kool:
-    print(i, end=' ')
+    print(i, end=' ') """
+
+#Задача 24
+n = int(input('Введите количество кустов: '))
+berry = list()
+for i in range(n):
+    x = int(input('Введите какое количество ягод выросло на кусте: '))
+    berry.append(x)
+berry_count = list()
+for i in range(len(berry) - 1):
+    berry_count.append(berry[i-1] + berry[i] + berry[i+1])
+berry_count.append(berry[-2] + berry[-1] + berry[0])
+print(max(berry_count))
